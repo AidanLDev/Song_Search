219bd0c5d28958ed89612cd1bd775efb2a68ee7c
AidanLDev <dev@aidanlowson.com>
2023-11-13 20:41:22 +0000

    Putting API in it's own dir

--- api/app.js
+++ api/app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/frontend', '/build')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/frontend', '/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../frontend', '/build', 'index.html'));
 });
 
 app.use('/songs', cors(), indexRouter);
