9d3dd411cffb6f926cf28f6de07890b3f57d73a2
AidanLDev <dev@aidanlowson.com>
2023-11-13 22:30:33 +0000

    Getting rid of unecessary route

--- api/app.js
+++ api/app.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
@@ -12,11 +11,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../frontend', '/build')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend', '/build', 'index.html'));
-});
 
 app.use('/songs', cors(), indexRouter);
 
