a4cf738d2b6e76cb3dde71e3618304b22b136b5a
AidanLDev <dev@aidanlowson.com>
2023-11-16 22:03:15 +0000

    Music library functionality

--- next-frontend/app/components/ArtistsDropdown.tsx
+++ next-frontend/app/components/ArtistsDropdown.tsx
@@ -3,13 +3,25 @@ import { Menu, MenuItem } from "@mui/material";
 interface IArtistDropdownProps {
   artists: string[];
   open: boolean;
+  close: () => void;
+  selectArtist: (artist: string) => void;
 }
 
-export const ArtistsDropdown = ({ artists, open }: IArtistDropdownProps) => {
+export const ArtistsDropdown = ({
+  artists,
+  open,
+  close,
+  selectArtist,
+}: IArtistDropdownProps) => {
   return (
-    <Menu open={open}>
+    <Menu open={open} onClose={close}>
       {artists.map((artist, idx) => (
-        <MenuItem key={`${artist}__${idx}`}>{artist}</MenuItem>
+        <MenuItem
+          key={`${artist}__${idx}`}
+          onClick={() => selectArtist(artists[idx])}
+        >
+          {artist}
+        </MenuItem>
       ))}
     </Menu>
   );
--- next-frontend/app/page.tsx
+++ next-frontend/app/page.tsx
@@ -1,34 +1,57 @@
 "use client";
-import Image from "next/image";
 import styles from "./page.module.css";
 import data from "../public/data/tracks.json";
 import { getAllArtists, getByTitle, getByArtist } from "@/utils/AppUtils";
 import { Button } from "@mui/material";
 import { ArtistsDropdown } from "./components/ArtistsDropdown";
 import { useState } from "react";
+import { ITrack } from "@/interfaces/tracks";
 
 export default function Home() {
   const [openArtistsDropdown, setOpenArtistsDropdown] = useState(false);
+  const [activeTracks, setActiveTracks] = useState<ITrack[] | undefined>();
   const allArtists = getAllArtists(data.tracks);
   const getTrack = getByTitle(data.tracks, "Cold Harbour Woman");
   const artist = getByArtist(data.tracks, "3am");
 
+  const handleOpenArtistsDropdown = () => {
+    setOpenArtistsDropdown(!openArtistsDropdown);
+  };
+
+  const handleCloseArtistsDropdown = () => {
+    setOpenArtistsDropdown(false);
+  };
+
+  const handleSelectArtist = (artist: string) => {
+    setActiveTracks(getByArtist(data.tracks, artist));
+    setOpenArtistsDropdown(false);
+  };
+
   return (
     <main className={styles.main}>
       <Button
-        onClick={() =>
-          setOpenArtistsDropdown((prevState) => prevState !== prevState)
-        }
+        onClick={handleOpenArtistsDropdown}
+        aria-expanded={openArtistsDropdown ? "true" : undefined}
+        aria-haspopup="true"
       >
         Select Artist
       </Button>
-      <ArtistsDropdown artists={allArtists} open={openArtistsDropdown} />
-
-      <ul>
-        {allArtists.map((artist, idx) => (
-          <li key={`${artist}__${idx}`}>{artist}</li>
-        ))}
-      </ul>
+      <ArtistsDropdown
+        artists={allArtists}
+        open={openArtistsDropdown}
+        close={handleCloseArtistsDropdown}
+        selectArtist={handleSelectArtist}
+      />
+      {activeTracks && activeTracks.length > 0 && (
+        <div>
+          {<p>{activeTracks[0].artist}</p>}
+          {activeTracks.map((track) => (
+            <div key={track.id}>
+              <span>{track.title}</span>
+            </div>
+          ))}
+        </div>
+      )}
     </main>
   );
 }
