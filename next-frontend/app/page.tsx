3fdbe6ac4b0c68cd6713fc9a420e9420aba4b9ab
AidanLDev <dev@aidanlowson.com>
2023-11-16 19:52:36 +0000

    helper functions

--- next-frontend/app/page.tsx
+++ next-frontend/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import data from "../public/data/tracks.json";
-import { getAllArtists, getByTitle } from "@/utils/AppUtils";
+import { getAllArtists, getByTitle, getByArtist } from "@/utils/AppUtils";
 
 export default async function Home() {
   const allArtists = getAllArtists(data.tracks);
-  const getTrack = getByTitle(data.tracks, "12 Stone Toddler");
-  console.log(getTrack);
+  const getTrack = getByTitle(data.tracks, "Cold Harbour Woman");
+  const artist = getByArtist(data.tracks, "3am");
+
   return (
     <main className={styles.main}>
       <ul>
--- next-frontend/utils/AppUtils.ts
+++ next-frontend/utils/AppUtils.ts
@@ -4,8 +4,12 @@ export const getAllArtists = (tracks: ITrack[]) => {
   return [...new Set(tracks.map((track) => track.artist))];
 };
 
-export const getByTitle = (tracks: ITrack[], songTitle: string) => {
-  return tracks.find(
-    (track) => track.title.toLowerCase() === songTitle.toLowerCase()
+export const getByArtist = (tracks: ITrack[], artist: string) => {
+  return tracks.filter(
+    (track) => track.artist.toLowerCase() === artist.toLowerCase()
   );
+};
+
+export const getByTitle = (tracks: ITrack[], songTitle: string) => {
+  return tracks.find((track) => track.title === songTitle);
 };
