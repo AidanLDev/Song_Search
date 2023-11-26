3e5df1952b86c6234a92306edf0472d5cc6b6b39
AidanLDev <dev@aidanlowson.com>
2023-11-26 11:03:37 +0000

    Responsive design

--- app/components/Title.tsx
+++ app/components/Title.tsx
@@ -6,7 +6,7 @@ export const Title = () => {
     <Typography
       color="primary"
       variant="h1"
-      className="text-center shadow-2xl underline decoration-[#DA25DC] font-outline"
+      className="text-center shadow-2xl underline decoration-[#DA25DC] font-outline sm:text-8xl text-7xl"
     >
       Track Search
     </Typography>
